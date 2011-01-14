f09c171cf05c527c883c348223a8a4e1273544df
eneiand <neil.t.anderson@gmail.com>
2011-01-14 23:00:07 +0000

    Baseline Structure

---
+++ Baseline/Baseline/ObjectCreation/MethodCall.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace Baseline.ObjectCreation
+{
+    private class MethodCall
+    {
+        private MethodInfo m_Method;
+        private List<ObjectInstance> m_Arguments;
+
+    }
+}
